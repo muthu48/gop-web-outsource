8f0ed9dd4fbde716a6a946cbfdaffe2d84886d65
dhana <dhana@LAPTOP-83IHS9NJ.fios-router.home>
2019-01-20 17:02:28 -0500

    Create Post with related file. download related file with file id.

--- src/main/java/com/jpw/springboot/controller/PostManagementController.java
+++ src/main/java/com/jpw/springboot/controller/PostManagementController.java
@@ -1,5 +1,7 @@
 package com.jpw.springboot.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,22 +9,33 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jpw.springboot.model.Post;
 import com.jpw.springboot.service.PostService;
 import com.jpw.springboot.service.UserService;
-//import com.jpw.springboot.util.CustomErrorType;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSFile;
 
 @RestController
 @RequestMapping("/post")
@@ -34,6 +47,10 @@ public class PostManagementController {
 
 	@Autowired
 	UserService userService;
+
+	@Autowired
+	GridFsOperations gridOperations;
+
 	@RequestMapping(value = "/getAllPosts", method = RequestMethod.GET)
 	public ResponseEntity<List<Post>> listAllPosts() {
 		List<Post> post = postService.findAllPosts();
@@ -47,39 +64,87 @@ public class PostManagementController {
 	public ResponseEntity<?> getPost(@PathVariable("id") String id) {
 		logger.info("Fetching Post with id {}", id);
 		Post post = postService.findById(id);
-		/*if (post == null) {
-			logger.error("Post with id {} not found.", id);
-			return new ResponseEntity(new CustomErrorType("Post with id " + id + " not found"), HttpStatus.NOT_FOUND);
-		}*/
+		/*
+		 * if (post == null) { logger.error("Post with id {} not found.", id); return
+		 * new ResponseEntity(new CustomErrorType("Post with id " + id + " not found"),
+		 * HttpStatus.NOT_FOUND); }
+		 */
 		return new ResponseEntity<Post>(post, HttpStatus.OK);
 	}
 
-    //@CrossOrigin(origins = "http://localhost:3000")
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	public ResponseEntity<?> createPost(@RequestBody Post post, UriComponentsBuilder ucBuilder) {
-		logger.info("Creating Post : {}", post);
+	@RequestMapping(value = "downloadFile/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getFileRelatedToPost(@PathVariable("id") String id) {
+		logger.info("Fetching file  with id {}", id);
 
-/*		if (postService.isPostExist(post)) {
-			logger.error("Unable to create. A Post with name {} already exist", post.getUserId());
-			return new ResponseEntity<Object>(
-					new CustomErrorType("Unable to create. A Post with name " + post.getUserId() + " already exist."),
-					HttpStatus.CONFLICT);
+		// BasicDBObject query = new BasicDBObject("metadata.postId", id);
+		GridFSDBFile file = gridOperations.findOne(new Query(Criteria.where("_id").is(id)));
+		
+		if(file !=null) {
+			return ResponseEntity.ok().contentType(MediaType.valueOf(file.getContentType()))
+				.body(new InputStreamResource(file.getInputStream()));
 		}
-*/		
-		post = postService.createPost(post);
+		return new ResponseEntity<Post>(HttpStatus.NO_CONTENT);
+		
+
+	}
+
+	// @CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> createPost(@RequestParam(value ="file" , required = false) MultipartFile file, @RequestParam("post") String postData,
+			UriComponentsBuilder ucBuilder) {
+		logger.info("Creating Post : {}", postData);
+		/*
+		 * if (postService.isPostExist(post)) {
+		 * logger.error("Unable to create. A Post with name {} already exist",
+		 * post.getUserId()); return new ResponseEntity<Object>( new
+		 * CustomErrorType("Unable to create. A Post with name " + post.getUserId() +
+		 * " already exist."), HttpStatus.CONFLICT); }
+		 */
+		ObjectMapper mapper = new ObjectMapper();
+		Post post = null;
+		InputStream inputStream = null;
+		try {
+			post = mapper.readValue(postData, Post.class);
+			post = postService.createPost(post);
+			if (file != null) {
+				String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+				inputStream = file.getInputStream();
+				DBObject metaData = new BasicDBObject();
+				metaData.put("postId", post.getId());
+				metaData.put("userId", post.getUserId());
+				GridFSFile gridFsFile = gridOperations.store(inputStream, fileName, "image/png", metaData);
+				//List<String> existingRelatedFiles = post.getRelatedFiles();
+				List<String> relatedFiles  = new ArrayList<String>();
+				relatedFiles.add(gridFsFile.getId().toString());
+				post.setRelatedFiles(relatedFiles);
+				postService.updatePost(post);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		System.out.println("Done");
 
 		HttpHeaders headers = new HttpHeaders();
-		headers.setLocation(ucBuilder.path("/api/post/{id}")
-				.buildAndExpand(post.getUserId()).toUri());
+		headers.setLocation(ucBuilder.path("/api/post/{id}").buildAndExpand(post.getUserId()).toUri());
 		headers.setAccessControlAllowOrigin("*");
 		headers.setAccessControlAllowCredentials(true);
 		List<HttpMethod> allowedMethods = new ArrayList<HttpMethod>();
 		allowedMethods.add(HttpMethod.POST);
 		headers.setAccessControlAllowMethods(allowedMethods);
-String[] allowedHeadersStrArr = {"Content-Type", "Accept", "X-Requested-With"};
-/*List<String> allowedHeaders = new ArrayList<String>();
-allowedHeaders.*/
-List<String> allowedHeaders = Arrays.asList(allowedHeadersStrArr) ;
+		String[] allowedHeadersStrArr = { "Content-Type", "Accept", "X-Requested-With" };
+		/*
+		 * List<String> allowedHeaders = new ArrayList<String>(); allowedHeaders.
+		 */
+		List<String> allowedHeaders = Arrays.asList(allowedHeadersStrArr);
 
 		headers.setAccessControlAllowHeaders(allowedHeaders);
 		return new ResponseEntity<Post>(post, HttpStatus.CREATED);
@@ -90,13 +155,12 @@ List<String> allowedHeaders = Arrays.asList(allowedHeadersStrArr) ;
 		logger.info("Updating Post with id {}", id);
 
 		Post currentPost = postService.findById(id);
-/*
-		if (currentPost == null) {
-			logger.error("Unable to update. Post with id {} not found.", id);
-			return new ResponseEntity<Object>(
-					new CustomErrorType("Unable to upate. Post with id " + id + " not found."), HttpStatus.NOT_FOUND);
-		}
-*/
+		/*
+		 * if (currentPost == null) {
+		 * logger.error("Unable to update. Post with id {} not found.", id); return new
+		 * ResponseEntity<Object>( new CustomErrorType("Unable to upate. Post with id "
+		 * + id + " not found."), HttpStatus.NOT_FOUND); }
+		 */
 		currentPost.setUserId(post.getUserId());
 		currentPost.setPostType(post.getPostType());
 		currentPost.setPostText(post.getPostText());
@@ -113,11 +177,12 @@ List<String> allowedHeaders = Arrays.asList(allowedHeadersStrArr) ;
 		logger.info("Fetching & Deleting Post with id {}", id);
 
 		Post post = postService.findById(id);
-/*		if (post == null) {
-			logger.error("Unable to delete. Post with id {} not found.", id);
-			return new ResponseEntity<Object>(
-					new CustomErrorType("Unable to delete. Post with id " + id + " not found."), HttpStatus.NOT_FOUND);
-		}*/
+		/*
+		 * if (post == null) {
+		 * logger.error("Unable to delete. Post with id {} not found.", id); return new
+		 * ResponseEntity<Object>( new CustomErrorType("Unable to delete. Post with id "
+		 * + id + " not found."), HttpStatus.NOT_FOUND); }
+		 */
 		postService.deletePostById(id);
 		return new ResponseEntity<Post>(HttpStatus.OK);
 	}
